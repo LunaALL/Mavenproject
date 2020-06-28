f2f3cc87aa0cdacffbac3e8a04084813f6dc7bb3
Lunall <wooju407@naver.com>
2020-06-28 22:05:56 +0900

    dd

--- sp5-chap08stu/src/main/java/config/Appctx.java
+++ sp5-chap08stu/src/main/java/config/Appctx.java
@@ -1,5 +1,27 @@
 package config;
 
-public class Appctx {
 
+
+
+
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Appctx {
+	
+	@Bean
+	public DataSource dataSource(){
+		DataSource ds = new DataSource();
+		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/spring5fs?characterEncoding=utf8");
+		ds.setUsername("spring5");
+		ds.setPassword("spring5");
+		ds.setInitialSize(2);
+		ds.setMaxActive(10);
+		return ds;
+		
+	}
 }
--- sp5-chap08stu/src/main/java/spring/MemberDao.java
+++ sp5-chap08stu/src/main/java/spring/MemberDao.java
@@ -1,5 +1,26 @@
 package spring;
 
+import java.sql.Connection;
+import java.util.Collection;
+
 public class MemberDao {
+	
+	public class MemberDDao {
+		
+		public Member selectByEmail(String email) {
+			return null;	
+		}
+		
+		public void insert(Member member) {
+			
+		}
+		public void update(Member member) {
+			
+		}
+		
+		public Collection<Member> selectAll() {
+			return null;
+		}
+	}
 
 }
