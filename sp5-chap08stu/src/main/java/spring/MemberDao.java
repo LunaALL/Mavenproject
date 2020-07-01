5df88b9d7c658e63daea278182e3a46957d3f96e
Lunall <wooju407@naver.com>
2020-07-01 19:40:30 +0900

    dd

--- sp5-chap08stu/src/main/java/spring/MemberDao.java
+++ sp5-chap08stu/src/main/java/spring/MemberDao.java
@@ -1,11 +1,16 @@
 package spring;
 
+
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
+import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class MemberDao {
 	
@@ -17,18 +22,53 @@ public class MemberDao {
 	
 
 	public Member selectByEmail(String email) {
-			return null;	
+		List<Member> results = jdbcTemplate.query("select * from MEMBER where EMAIL =?",
+				new RowMapper<Member>() {
+					@Override
+					public Member mapRow(ResultSet rs ,int rownum) throws SQLException{
+						Member member = new Member(
+								rs.getString("EMAIL"),
+								rs.getString("PASSWORD"),
+								rs.getString("NAME"),
+								rs.getTimestamp("REGDATE").toLocalDateTime());
+						member.setId(rs.getLong("ID"));
+						return member;
+					}
+		},email);
+			return  results.isEmpty() ? null : results.get(0);
 		}
 		
 	public void insert(Member member) {
 			
 		}
 	public void update(Member member) {
+		jdbcTemplate.update("update MEMBER set NAME=?, PASSWORD=? where EMAIL = ?", member.getName(), member.getPassword(),member.getEmail());
 			
 		}
 		
 	public Collection<Member> selectAll() {
-			return null;
+		
+		List<Member> results = jdbcTemplate.query("select * from MEMBER",
+				new RowMapper<Member>() {
+					@Override
+					public Member mapRow(ResultSet rs ,int rownum) throws SQLException{
+						Member member = new Member(
+								rs.getString("EMAIL"),
+								rs.getString("PASSWORD"),
+								rs.getString("NAME"),
+								rs.getTimestamp("REGDATE").toLocalDateTime());
+						member.setId(rs.getLong("ID"));
+						return member;
+					}
+		});
+			return  results;
+		
+			
 		}
+	
+	public int count() {
+		Integer count = jdbcTemplate.queryForObject("select count(*) from MEMBER", Integer.class);
+		return count;
+	}
 	
 }
