8d0a902419e344319759af625640c50f05757074
Lunall <wooju407@naver.com>
2020-07-06 20:33:23 +0900

    dd

---
+++ sp-chap09/src/main/java/config/MvcConfig.java
@@ -0,0 +1,23 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configurable
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/view/", ".jsp");
+	}
+
+}
---
+++ sp-chap09/src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>스프링 웹 mvc 테스트</title>
+</head>
+<body>
+    인사말 : ${greeting }
+
+</body>
+</html>
